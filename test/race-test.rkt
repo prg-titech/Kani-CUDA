4eb26a0f64048c18eb97d8d5409e8fa8836a0177
Akira Kani <akira@Akira-no-MacBook-Pro.local>
2016-10-12 19:15:13 +0900

    Add a new function for race test between blocks

--- test/race-test.rkt
+++ test/race-test.rkt
@@ -1,10 +1,25 @@
 #lang rosette
 
-(require "lang.rkt")
+(require "../lang.rkt")
 
-(define arr (make-array (for/vector ([i (in-range 10)]) i)))
+(define arr (make-array (for/vector ([i (in-range 10)]) (make-element 0))))
 
 (define (test arr)
   (:= int x [arr 0])
-  (if- (eq?/LS (tid) (ntid))
-         (= [arr 0] 1)))
+  (if- (eq?/LS (tid) (- (ntid) 1))
+       (= [arr 0] 1)))
+
+(define (test-block-race arr)
+  (: int x) 
+  (if (eq? (bid) 0)
+       (begin
+         (if- (eq?/LS (tid) 0)
+              (= [arr 0] 4))
+         (barrier)
+         (if- (eq?/LS (tid) 1)
+              (= x [arr 0])))
+       (= x [arr 0])))
+
+(invoke-kernel test-block-race 2 5 arr)
+
+(for ([i (in-range 10)]) (print (element-content (vector-ref (array-contents arr) i))))
