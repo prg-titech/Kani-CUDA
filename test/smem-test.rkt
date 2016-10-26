8fad606e4d644ea4c68d988cf590decee47d6cf1
Akira Kani <akira@Akira-no-MacBook-Pro.local>
2016-10-27 01:45:40 +0900

    Add a test file for shared memory

---
+++ test/smem-test.rkt
@@ -0,0 +1,15 @@
+#lang rosette
+
+(require "../lang.rkt")
+
+(define (test-smem src)
+  (:shared int arr[10][1])
+  (:= int ix (+/LS (tid) (*/LS (bid) (thread-dim))))
+  (= [arr (tid)] [src ix])
+  (barrier)
+  (print [arr 0 0])
+)
+
+(define src (make-array (for/vector ([i (in-range 30)]) (make-element i)) 30))
+
+(invoke-kernel test-smem 3 10 src)
