6586ec8690b5d06bd3ed9ab247e2b04ae4e52a9e
masuhara <masuhara@is.titech.ac.jp>
2019-06-25 13:02:33 +0900

    synth.sh now checks whether main.rkt succeeded

--- Synthesizer/synth.sh
+++ Synthesizer/synth.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-racket main.rkt $1
-racket out.rkt
+racket main.rkt $1 || {
+    c=$?;
+    echo "Converting $1 to KaniCUDA failed.";
+    exit $c;
+}
+racket out.rkt || {
+    c=$?;
+    echo "Converting $1 to KaniCUDA failed.";
+    exit $c;
+}
 echo Synthesizing...
 java -classpath ../Solver/kani-cuda/bin Expression.Main
